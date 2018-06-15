43153041f1e16aa7655c4b3acfaf8bd00356c932
Sean James Cosentino <sparkcollaboration@gmail.com>
2018-06-15 13:20:31 -0400

    basic user conditional and stock search

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,5 +1,18 @@
 class StaticpagesController < ApplicationController
   def home
+    
+      if params[:id] == ""
+        @nothing = "Enter a Valid Symbol"
+      elsif
+        if params[:id]
+          begin
+            @stock = StockQuote::Stock.quote(params[:id])
+          rescue
+            @error = "Check your search and try again."
+          end
+        end
+      end
+
   end
 
   def about
@@ -7,6 +20,6 @@ class StaticpagesController < ApplicationController
 
   def contact
   end
-  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'staticpages#home'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'staticpages#home', as: :authenticated_user
+    end
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+
+
+
   get '/about' => 'staticpages#about'
+  get '/' => 'staticpages#home', as: :dashboard
+  post '/' => 'staticpages#home'
+
 end
