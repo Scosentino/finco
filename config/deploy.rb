c4f07557a688ef7f57f9791779cf3603fd103123
Sean James Cosentino <sparkcollaboration@gmail.com>
2018-06-26 10:26:01 -0400

    commiting

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,4 +6,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/application.yml')
 set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
 set :sidekiq_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :sidekiq_concurrency, 10
+set :sidekiq_default_hooks, true
 set :keep_releases, 5
