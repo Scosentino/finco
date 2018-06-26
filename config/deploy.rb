31f013f19aec8a1cadf5be19640353204ad300a3
Sean James Cosentino <sparkcollaboration@gmail.com>
2018-06-26 10:20:57 -0400

    adding files for worekrs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,6 @@ set :repo_url, 'git@github.com:Scosentino/finco.git'
 set :pty, true
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/application.yml')
+set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
+set :sidekiq_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :keep_releases, 5
